8024b3bcd32167b07ab186acb122419b7d35b753
Angela <angelafaria34@gmail.com>
2021-10-28 00:11:24 -0300

    Completei o script consultarPet (Get)

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -11,23 +11,25 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
 
 public class Pet {
     // 3.1 - Atributos
-    String uri = "https://petstore.swagger.io/v2/pet"; // endere√ßo da entidade Pet
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereÁo da entidade Pet
 
-    // 3.2 - M√©todos e Fun√ß√µes
+    // 3.2 - MÈtodos e FunÁıes
     public String lerJson(String caminhojson) throws IOException {
 
         return new String(Files.readAllBytes(Paths.get(caminhojson)));
     }
     // Incluir - creater - Post
-    @Test // identifica o m√©todo ou fun√ß√£o como um teste para o TestNG
+    @Test // identifica o mÈtodo ou funÁ„o como um teste para o TestNG
     public void incluitPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe - Gherkin
-        // Dado - Quando - Ent√£o
+        // Dado - Quando - Ent„o
         // Given - When - Then
 
         given() // Dado
@@ -36,13 +38,42 @@ public class Pet {
                 .body(jsonBody)
         .when() // Quando
                 .post(uri)
-        .then() // Ent√£o
+        .then() // Ent„o
                 .log().all()
                 .statusCode(200)
+                .body("name",is("Bob"))
+                .body("status", is ("available"))
+                .body("category.name", is("AZ2473LORD"))
+                .body("tags.name",contains("data"))
+
         ;
 
 
     }
+    @Test
+    public void consultarpet(){
+        String petId = "1979070653";
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Bob"))
+                .body("category.name", is("AZ2473LORD"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
 
 
-}
+        ;
+        System.out.println("o token È " + token);
+    }
+
+    }
+
+
+
